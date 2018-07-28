77b1ae6318baad4e239a310f960d871b8dc0c18c
binote00 <jfvanass@gmail.com>
2018-07-28 11:21:14 +0200

    fix admin_city urls

--- admin/admin_city_recce.php
+++ admin/admin_city_recce.php
@@ -1,11 +1,11 @@
-<?
-require_once('./jfv_inc_sessions.php');
-include_once('./jfv_include.inc.php');
-if(!$Admin)$Admin=GetData("Joueur","ID",$_SESSION['AccountID'],"Admin");
-if($Admin ==1) {
+<?php
+require_once '../jfv_inc_sessions.php';
+include_once '../jfv_include.inc.php';
+if (!$Admin) $Admin = GetData("Joueur", "ID", $_SESSION['AccountID'], "Admin");
+if ($Admin == 1) {
     dbconnect();
     $result = $dbh->prepare("UPDATE Lieu SET Recce=1 WHERE ID=:id");
-    $result->bindValue('id',$_GET['id'],1);
+    $result->bindValue('id', $_GET['id'], 1);
     $result->execute();
-    header('Location: em_city_ground.php?id='.$_GET['id']);
+    header('Location: ../em_city_ground.php?id=' . $_GET['id']);
 }
--- admin/admin_city_reveal.php
+++ admin/admin_city_reveal.php
@@ -1,14 +1,14 @@
-<?
-require_once('./jfv_inc_sessions.php');
-include_once('./jfv_include.inc.php');
-if(!$Admin)$Admin=GetData("Joueur","ID",$_SESSION['AccountID'],"Admin");
-if($Admin ==1) {
+<?php
+require_once '../jfv_inc_sessions.php';
+include_once '../jfv_include.inc.php';
+if (!$Admin) $Admin = GetData("Joueur", "ID", $_SESSION['AccountID'], "Admin");
+if ($Admin == 1) {
     dbconnect();
     $result = $dbh->prepare("
     UPDATE Regiment_IA r, Pays p SET r.Visible=1
     WHERE r.Pays=p.ID AND r.Lieu_ID=:id AND p.Faction=:faction AND r.Vehicule_Nbr >0");
-    $result->bindValue('id',$_GET['id'],1);
-    $result->bindValue('faction',$_GET['f'],1);
+    $result->bindValue('id', $_GET['id'], 1);
+    $result->bindValue('faction', $_GET['f'], 1);
     $result->execute();
-    header('Location: em_city_ground.php?id='.$_GET['id']);
+    header('Location: ../em_city_ground.php?id=' . $_GET['id']);
 }
